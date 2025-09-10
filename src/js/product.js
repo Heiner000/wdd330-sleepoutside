d9e89151846799b31d72bb4034806d2e5516ffeb
Douglas Justice <Kobbybhills462@gmail.com>
2025-09-10 07:02:27 +0000

    fixed the product.js

--- src/js/product.js
+++ src/js/product.js
@@ -1,9 +1,14 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
+  // 1st Step. Getting the current cart (or empty array assuming nothing is stored yet)
+  let cart = getLocalStorage("so-cart") || [];
+  // 2nd step. Adding the new product to the array
+  cart.push(product);
+  // 3rd step. Saving the updated cart back into localStorage
   setLocalStorage("so-cart", product);
 }
 // add to cart button event handler
